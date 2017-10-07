e4a460662ce89537ebcdffc806d839483d68b000
Corwin Belser <cmb539@drexel.edu>
2017-10-07 18:17:09 -0400

    Initial Unity project. Playground folder contains first pass at line-of-sight enemy.

---
+++ Assets/_SCRIPTS/line_of_sight_ai.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class line_of_sight_ai : MonoBehaviour {
+
+    public int MAX_DISTANCE; /* Maximum distance raycast should travel */
+    
+    private GameObject _player;
+    private NavMeshAgent _agent;
+
+
+	// Use this for initialization
+	void Start () {
+        _agent = GetComponent<NavMeshAgent>();
+        _player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        Physics.Raycast(transform.position, _player.transform.position - transform.position, out hit, MAX_DISTANCE);
+
+        if (hit.collider != null && hit.collider.gameObject != null && hit.collider.gameObject.CompareTag("Player"))
+            _agent.SetDestination(_player.transform.position);
+	}
+}
