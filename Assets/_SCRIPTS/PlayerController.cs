0e249d531b4f91da750e4d97ecda02574ca3aa9e
no-na <noahknam@gmail.com>
2017-10-09 05:49:59 -0400

    Added Character controller in top down perspective.

---
+++ Assets/_SCRIPTS/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent( typeof( BoxCollider2D ), typeof( Rigidbody2D ) )]
+public class PlayerController : MonoBehaviour {
+	[SerializeField]
+	private Rigidbody _rb;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetAxisRaw("Horizontal")>0 )
+		{
+			_rb.velocity = new Vector3(speed,0,_rb.velocity.z);
+        }
+		if( Input.GetAxisRaw("Horizontal")<0 )
+		{
+			_rb.velocity = new Vector3(-speed,0,_rb.velocity.z);
+        }
+		if( Input.GetAxisRaw("Vertical")>0)
+		{
+			_rb.velocity = new Vector3(_rb.velocity.x,0,speed);
+		}
+		if( Input.GetAxisRaw("Vertical")<0)
+		{
+			_rb.velocity = new Vector3(_rb.velocity.x,0,-speed);
+		}
+	}
+}
---
+++ Assets/_SCRIPTS/SmoothFollow.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow : MonoBehaviour
+	{
+	
+	[SerializeField]
+	private float _xMin = 0f;
+	[SerializeField]
+	private float _xMax = 0f;
+	[SerializeField]
+	private float _zMin = 0f;
+	[SerializeField]
+	private float _zMax = 0f;
+	[SerializeField]
+	private float _cameraSpeed = 0f;
+	
+	private float _yOffset;
+	private float _zOffset;
+	
+	
+	// The transforms of the players. Used to determine their midpoint.
+	public Transform playerTransform;
+	
+	void Start(){
+		_yOffset = transform.position.y - playerTransform.position.y;
+		_zOffset = transform.position.z - playerTransform.position.z;
+	}
+		
+
+	void Update(){
+		Vector3 target = new Vector3(
+			playerTransform.position.x,
+			playerTransform.position.y+_yOffset,
+			playerTransform.position.z+_zOffset
+		);
+		
+		float step = _cameraSpeed * Time.deltaTime;
+        transform.position = Vector3.Lerp(transform.position, target, step);
+		
+		// Apply clamps to position
+		transform.position = new Vector3(
+			Mathf.Clamp(transform.position.x, _xMin, _xMax),
+			_yOffset,
+			Mathf.Clamp(transform.position.z, _zMin, _zMax)
+		);
+	}
+}
