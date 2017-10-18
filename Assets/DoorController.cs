fe8daeaf1906299f1bd9a250c2d48db3987a917c
Joseph S. Lipinski <Joseph.S.Lipinski@drexel.edu>
2017-10-18 17:15:44 -0400

    Added door opening and closing on pressing f

---
+++ Assets/DoorController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+    public GameObject door;
+    public GameObject pivot;
+
+    bool touched = false;
+    string touchedBy;
+
+    //The starting rotation of the door in the Y direction
+    float startingRotation;
+    float openInwardsTo;
+    float openOutwardsTo;
+
+    enum DoorStates { closed, open };
+    DoorStates doorState;
+    // Use this for initialization
+    void Start ()
+    {
+        startingRotation = door.transform.rotation.y;
+        openOutwardsTo = startingRotation - 0.3f;
+        openInwardsTo = startingRotation + 0.3f;
+        
+        doorState = DoorStates.closed;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (touched)
+        {
+            if(doorState == DoorStates.closed)
+            {
+                if (door.transform.rotation.y < openInwardsTo)
+                {
+                    door.transform.RotateAround(pivot.transform.position, Vector3.up, -40 * Time.deltaTime);
+                }
+                else
+                {
+                    touched = false;
+                    doorState = DoorStates.open;
+                }
+            }
+            if (doorState == DoorStates.open)
+            {
+                if (door.transform.rotation.y  > startingRotation)
+                {
+                    door.transform.RotateAround(pivot.transform.position, Vector3.up, 40 * Time.deltaTime);
+                }
+                else
+                {
+                    touched = false;
+                    doorState = DoorStates.closed;
+                }
+            }
+        }
+    }
+
+    void ChangeDoorState()
+    {
+
+        touched = true;
+    }
+}
--- Assets/_SCRIPTS/PlayerController.cs
+++ Assets/_SCRIPTS/PlayerController.cs
@@ -2,29 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent( typeof( CapsuleCollider ), typeof( Rigidbody ) )]
+[RequireComponent(typeof(CapsuleCollider), typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour {
-	[SerializeField]
-	private Rigidbody _rb;
-	public float speed;
+    [SerializeField]
+    private Rigidbody _rb;
+    public float speed;
     public float jumpThrust = 1850;
     public bool _isDashing = false;
-	public Transform SAFEROOM_TRANSFORM;
+    public Transform SAFEROOM_TRANSFORM;
     Vector3 dashForce;
     Transform[] array;
 
     // Use this for initialization
-    void Start () 
-	{
-		
-	}
+    void Start()
+    {
+
+    }
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
-	    if(_isDashing == false)
+        if (_isDashing == false)
         {
-			Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("DashableWall"), false);
+            Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("DashableWall"), false);
             if (Input.GetAxisRaw("Horizontal") > 0)
             {
                 _rb.velocity = new Vector3(speed, 0, _rb.velocity.z);
@@ -51,14 +51,18 @@ public class PlayerController : MonoBehaviour {
                 dashForce = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
                 StartCoroutine(FadeWalls(this.gameObject.transform.position, 5.0f));
             }
+            if (Input.GetKeyDown("f"))
+            {
+                StartCoroutine(OpenDoors(this.gameObject.transform.position, 5.0f));
+            }
         }
         else
         {
-			Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("DashableWall"), true);
-			_rb.AddForce(dashForce * jumpThrust);
+            Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("DashableWall"), true);
+            _rb.AddForce(dashForce * jumpThrust);
         }
 
-	}
+    }
 
     public IEnumerator FadeWalls(Vector3 center, float radius)
     {
@@ -74,16 +78,32 @@ public class PlayerController : MonoBehaviour {
                 {
                     //hitColliders[i].SendMessage("Disable_Collision");
                     //print("Disable collision");
-                } 
+                }
             }
             i++;
             yield return null;
         }
-		yield return new WaitForSeconds(0.2f);
-		_isDashing = false;
-        StopCoroutine(FadeWalls(this.gameObject.transform.position, 4.0f));
+        yield return new WaitForSeconds(0.2f);
+        _isDashing = false;
+        StopCoroutine(FadeWalls(this.gameObject.transform.position, 2.0f));
     }
-	
+    public IEnumerator OpenDoors(Vector3 center, float radius)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            if (hitColliders[i].tag == "Door")
+            {
+                //print(hitColliders[i].name);
+                hitColliders[i].SendMessage("ChangeDoorState");    
+            }
+            i++;
+            yield return null;
+        }
+        yield return null;
+        StopCoroutine(OpenDoors(center, radius));
+    }
 	void OnCollisionEnter(Collision collision)
 	{
 		if(collision.gameObject.tag == "Enemy")
