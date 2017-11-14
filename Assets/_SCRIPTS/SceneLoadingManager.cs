bb1bca5806a9d1e2ae9d0d389bacfb85c0657cdc
Joe Lipinski <jsl94@drexel.edu>
2017-11-14 12:47:08 -0800

    Added Script for Level Loading
    Function takes in scene name as an arguemenent and loads that scene

---
+++ Assets/_SCRIPTS/SceneLoadingManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// Author: Joseph Lipinski
+/// Used for loading new Unity Scenes
+
+
+public class SceneLoadingManager : MonoBehaviour {
+
+	/// <summary> Loads the scene that is passed into it </summary>
+	/// <param name = SceneName> The name of the scene to be loaded </param>
+
+	void LoadScene(string SceneName)
+	{
+		SceneManager.LoadScene(SceneName, LoadSceneMode.Additive);
+	}
+}
