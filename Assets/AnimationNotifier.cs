546acbc91897b05f8c78f1f85c2134124ba24b14
no-na <noahknam@gmail.com>
2017-11-14 21:28:26 -0500

    -Rem properly dies. The screen fades to black before she teleports the respawn point.

---
+++ Assets/AnimationNotifier.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationNotifier : MonoBehaviour {
+	
+	public GameObject target;
+
+	public void Notify(string message){
+		target.SendMessage(message);
+	}
+}
