4b3ea350f131692f30a12eb9bb6752f995f0595f
Joe Lipinski <jsl94@drexel.edu>
2017-11-15 11:16:29 -0800

    Added Loading Level Prefab
    For all new levels to be strung together. Place the Level Loader prefab in them at the end of the level. Align the bottom of the collider with the bottom of the floor. When the momento is dropped it will hit the collider and cause a new level to be loaded. My hotfix was removed from memento.cs

--- Assets/_SCRIPTS/SceneLoadingManager.cs
+++ Assets/_SCRIPTS/SceneLoadingManager.cs
@@ -6,9 +6,27 @@ using UnityEngine.SceneManagement;
 /// Author: Joseph Lipinski
 /// Used for loading new Unity Scenes
 
+public class SceneLoadingManager : MonoBehaviour 
+{
 
-public class SceneLoadingManager : MonoBehaviour {
+	int BuildIndex, nextBuildIndex;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		BuildIndex = SceneManager.GetActiveScene().buildIndex;
+		nextBuildIndex = BuildIndex + 1;
+	}
 
+	/// The prefab has a collider. When the memento touches it the next level in the build index
+	void OnTriggerEnter(Collider collider)
+	{
+		if(collider.gameObject.tag == "Memento")
+		{
+			LoadScene(nextBuildIndex);
+		}
+	}
+	
 	/// <summary> Loads the scene that is passed into it </summary>
 	/// <param name = SceneName> The name of the scene to be loaded </param>
 
