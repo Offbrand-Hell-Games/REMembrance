23d87e2be565d87c572900a0f3148da669283d85
no-na <noahknam@gmail.com>
2017-11-14 21:34:27 -0500

    Added documentation to a script I wrote.

--- Assets/AnimationNotifier.cs
+++ Assets/AnimationNotifier.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// Author: Noah Nam
+/// This is meant to be used in animation events. Notify will send a message to the gameobject with the method to be called.
 public class AnimationNotifier : MonoBehaviour {
 	
 	public GameObject target;
 
+	/// <summary> Notify uses SendMessage to a target gameobject.</summary>
+	/// <param name=message> The name of the function you want to be run from the target gameobject.</param>
 	public void Notify(string message){
 		target.SendMessage(message);
 	}
