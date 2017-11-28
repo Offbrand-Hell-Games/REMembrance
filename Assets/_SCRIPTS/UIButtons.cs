496edbd3c5df1082a8f80f8cff403066dd7435c0
no-na <noahknam@gmail.com>
2017-11-28 07:07:25 -0500

    -Added Main Menu. -Added Options Menu. -Somewhat added pause menu, it still needs to be added to individual levels, since it would compete for a button with the preference panel, I wanted to meet about it for the Tusedaymeeting.

--- Assets/_SCRIPTS/UIButtons.cs
+++ Assets/_SCRIPTS/UIButtons.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 ///	Author: Noah Nam
 ///	UIButtons contains code called by UI Buttons.
 public class UIButtons : MonoBehaviour {
+	
+	public bool affectsHighlight = false;
+	private bool wasHovered;
+	private GameObject hoveredButton;
 	
 	/// <summary> Quits the application. </summary>
 	public void QuitGame()
@@ -18,5 +23,46 @@ public class UIButtons : MonoBehaviour {
 		#else
 			Application.Quit();
 		#endif
+	}
+	
+	/// <summary> For the highlight on buttons. </summary>
+	void Update(){
+		if (wasHovered){
+			if(affectsHighlight){
+				float step = 20f * Time.deltaTime;
+				Vector3 target = new Vector3(transform.position.x,hoveredButton.transform.position.y,transform.position.z);
+				transform.position = Vector3.Lerp(transform.position, target, step);
+			}
+			
+		}
+	}
+	
+	/// <summary> Sets a menu button to be highlighted. </summary>
+	/// <param name=b> The gameobject(menu button) to be highlighted </param>
+	public void TriggerHovered(GameObject b){
+		wasHovered = true;
+		hoveredButton = b;
+	}
+	
+	/// <summary> Starts the first level of the game. </summary>
+	public void StartGame(){
+		SceneManager.LoadScene(1, LoadSceneMode.Single);
+	}
+	
+	/// <summary> Returns to main menu. </summary>
+	public void MainMenu(){
+		SceneManager.LoadScene(0, LoadSceneMode.Single);
+	}
+	
+	/// <summary> Opens a menu by setting its gameobject active. </summary>
+	/// <param name=menu> The gameobject(menu button) to be opened. </param>
+	public void OpenMenu(GameObject menu){
+		menu.SetActive(true);
+	}
+	
+	/// <summary> Closes a menu by setting its gameobject dormant. </summary>
+	/// <param name=menu> The gameobject(menu button) to be closed. </param>
+	public void CloseMenu(GameObject menu){
+		menu.SetActive(false);
 	}
 }
