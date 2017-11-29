5d4d5c3a993e5f2b8b8cd9f75b09dcb3921263ce
no-na <noahknam@gmail.com>
2017-11-28 21:13:50 -0500

    -Added Pause Menu, this replaces the old pref screen. -Known issues: Volume Slider does always visually starts at 100%. Prefs pane must be hooked up manually for now, working on rework.

--- Assets/PrefsPaneManager.cs
+++ Assets/PrefsPaneManager.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class PrefsPaneManager : MonoBehaviour {
 	public static PrefsPaneManager instance;
 
+	public GameObject pause_menu;
 	public GameObject prefs_container;
 	public GameObject slider_prefab;
 	public GameObject OTS_camera;
-	public GameObject prefs_ui;	/// <param name=prefs_ui> The parent object that contains buttons used in the Prefs Pane.</param>
+	//public GameObject prefs_ui;	/// <param name=prefs_ui> The parent object that contains buttons used in the Prefs Pane.</param>
 //	public delegate void valueChangedDelegate(float newvalue);
 	//float importantValue = .7f;
 	// Use this for initialization
@@ -23,18 +24,19 @@ public class PrefsPaneManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Escape)) {
-			if (prefs_container.activeSelf) {
-				prefs_container.SetActive (false);
-				prefs_ui.SetActive(false);
+			if (pause_menu.activeSelf) {
+				pause_menu.SetActive (false);
+				prefs_container.SetActive(false);
+				//prefs_ui.SetActive(false);
 				Cursor.visible = false;
 			} else {
-				prefs_container.SetActive (true);
-				prefs_ui.SetActive(true);
+				pause_menu.SetActive (true);
+				//prefs_ui.SetActive(true);
 				Cursor.visible = true;
 			}
 		}
 
-		OTS_camera.GetComponent<OTSCamera3> ().freezeCam = prefs_container.activeSelf;
+		OTS_camera.GetComponent<OTSCamera3> ().freezeCam = pause_menu.activeSelf;
 
 //		if (Input.GetKeyDown (KeyCode.I) && false) {
 //			GameObject newslider = Instantiate (slider_prefab);
