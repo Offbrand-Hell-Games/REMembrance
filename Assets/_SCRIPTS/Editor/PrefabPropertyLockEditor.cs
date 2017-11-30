61b17c506af94478fac9c1ba2f313f10511c85e0
Corwin Belser <cmb539@drexel.edu>
2017-11-30 05:39:06 -0500

    Created editor script to lock active state of walls in tile prefabs. Has not been connected yet.

---
+++ Assets/_SCRIPTS/Editor/PrefabPropertyLockEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PrefabPropertyLock))]
+[CanEditMultipleObjects]
+public class PrefabPropertyLockEditor : Editor {
+
+    SerializedProperty ActiveStates;
+
+    public void OnEnable()
+    {
+        ActiveStates = serializedObject.FindProperty("test");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(ActiveStates);
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/_SCRIPTS/PrefabPropertyLock.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// Author: Corwin Belser
+/// Allows properties to be added to a list, preventing them from being overriden
+///     when a prefab change is applied
+[ExecuteInEditMode]
+public class PrefabPropertyLock : MonoBehaviour {
+
+    public Vector3 test;
+    public Dictionary<string, bool> ActiveStates;
+
+    public void Start()
+    {
+        if (ActiveStates == null)
+        {
+            ActiveStates = new Dictionary<string, bool>();
+
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                GameObject child = transform.GetChild(i).gameObject;
+                ActiveStates.Add(child.name, child.activeSelf);
+            }
+        }
+    }
+
+    public void Update()
+    {
+        foreach (string name in ActiveStates.Keys) {
+            Transform child = transform.Find(name);
+            if (child != null) {
+                bool value;
+                if (ActiveStates.TryGetValue(name, out value))
+                    child.gameObject.SetActive(value);
+            }
+        }
+    }
+}
