1f2cff64a504b93e4d5e0343e8b5e57c44e15b8a
Corwin Belser <cmb539@drexel.edu>
2018-01-20 23:51:54 -0500

    Created context menu item to quickly generate folders for a new asset

---
+++ Assets/Editor/CreateFolderMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Adds menu item when right-clicking in the project folder -> Create menu
+/// 
+/// Creates basic folder structure for a new asset
+/// </summary>
+public class CreateFolderMenu {
+
+    /* List of folders to create for the asset */
+    private static string[] _folders = { "Scripts", "Materials", "Imports", "Animations" };
+    
+    /// <summary>
+    /// Right-click in project view where asset should be created
+    /// Create -> Asset Folders
+    /// </summary>
+    [MenuItem("Assets/Create/Asset Folders", priority = 21)]
+	private static void CreateFoldersForAsset()
+    {
+        Object selectedAsset = Selection.activeObject;
+        if (selectedAsset != null)
+        {
+            string filePath = AssetDatabase.GetAssetPath(selectedAsset);
+            if (System.IO.Directory.Exists(filePath))
+            {
+                string assetGUID = AssetDatabase.CreateFolder(filePath, "New Asset");
+                string assetFilePath = AssetDatabase.GUIDToAssetPath(assetGUID);
+
+                foreach (string folder in _folders)
+                {
+                    AssetDatabase.CreateFolder(assetFilePath, folder);
+                }
+
+                /* Create a Readme file for the asset */
+                System.IO.File.CreateText(assetFilePath + "/Readme.txt");
+            }
+        }
+    }
+}
